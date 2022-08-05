5c7eee519f1416a57d11758d3800b7d31ee36d2a
Anton Samarchyan <desecho@gmail.com>
2022-08-05 13:12:24 -0400

    Refactoring

---
+++ frontend/src/components/ToolbarButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn icon>
+    <v-icon :color="color">mdi-{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ToolbarButton",
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    color(): string {
+      if (this.active) {
+        return "grey";
+      }
+      return "white";
+    },
+  },
+});
+</script>
