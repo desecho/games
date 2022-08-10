fe71cd43980828879ea13753f5806977860f0f98
Anton Samarchyan <desecho@gmail.com>
2022-08-10 13:43:46 -0400

    Improve types

--- frontend/src/composables/formValidation.ts
+++ frontend/src/composables/formValidation.ts
@@ -1,23 +1,18 @@
 import { ref } from "vue";
 
-import type { FormValidationResult } from "./types";
+import type { Form } from "./types";
 import type { Ref } from "vue";
 
 export function useFormValidation(): {
-  form: Ref<null>;
+  form: Ref<Form | null>;
   isValid: () => Promise<boolean>;
 } {
-  // Not sure how to type this
-  const form = ref(null);
+  const form = ref(null) as Ref<Form | null>;
 
   async function isValid(): Promise<boolean> {
-    const value = form.value;
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (value !== null) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      const result = (await value.validate()) as FormValidationResult;
-      const valid: boolean = result.valid;
-      return valid;
+    if (form.value !== null) {
+      const result = await form.value.validate();
+      return result.valid;
     }
     return false;
   }
