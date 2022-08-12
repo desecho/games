b4fd178b4c6e569f8caa6d6c623db9066cf4f984
Anton Samarchyan <desecho@gmail.com>
2022-08-12 09:33:38 -0700

    Refactoring

--- frontend/src/components/GamesTabs.vue
+++ frontend/src/components/GamesTabs.vue
@@ -1,19 +1,26 @@
 <template>
-  <v-tab v-for="list in lists" :key="list.id" :value="list.key" :to="getPath(list.key)" :title="list.name">
-    <v-icon>mdi-{{ list.icon }}</v-icon>
-    <span v-if="!isPhone">{{ list.name }}</span>
-  </v-tab>
+  <v-tabs v-model="tab" background-color="primary" centered stacked>
+    <v-tab v-for="list in lists" :key="list.id" :value="list.key" :to="getPath(list.key)" :title="list.name">
+      <v-icon>mdi-{{ list.icon }}</v-icon>
+      <span v-if="!isPhone">{{ list.name }}</span>
+    </v-tab>
+  </v-tabs>
 </template>
 
 <script lang="ts" setup>
+import { toRef } from "vue";
+
 import { useMobile } from "../composables/mobile";
 import { Lists } from "../const";
+import { useGamesStore } from "../stores/games";
 
 const props = defineProps<{
   username?: string;
 }>();
 
 const lists = Lists;
+const gamesStore = useGamesStore();
+const tab = toRef(gamesStore, "tab");
 
 function getPath(listKey: string): string {
   if (props.username !== undefined) {
